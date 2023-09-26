b75b566750d7194067cda48878a63cbbeb62e728
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-09-26 20:23:08 +0200

    Add files via upload

---
+++ SPNP/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPNP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ThreadingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new ThreadingWindow().ShowDialog();
+            this.Show();
+        }
+
+        private void SynchronisationButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new SynchroWindow().ShowDialog();
+            this.Show();
+        }
+
+        private void MultitaskingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new MultiTasking().ShowDialog();
+            this.Show();
+        }
+
+        private void CancelingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new CancelingWindow().ShowDialog();
+            this.Show();
+        }
+
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new ProcessWindow().ShowDialog();
+            this.Show();
+        }
+    }
+}
